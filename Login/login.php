b8504abbf901e82e26f3dc75c6b659ab26132b30
Stefan Khor <stefankhor2002@gmail.com>
2023-06-09 16:01:11 +0800

    update login

---
+++ Login/login.php
@@ -0,0 +1,27 @@
+
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href = "style.css">
+    </head>
+
+    <body>
+        <div class="container">
+
+        <!-- left part div -->
+            <div class="leftdiv">
+                <img src="../sfiles/MMU_Logo.png" alt="MMU Logo" >
+            </div>
+
+        <!-- right part div -->
+            <form action="Form submission" method="post">
+                <div class = "rightdiv">            
+                    <h2>Sign In</h2>
+                    <input type="text" name="username" placeholder = "Username">
+
+                    <input type="password" name="password" placeholder = "Password">
+
+                    <button name="LoginBtn">Login</button>
+                </div>
+            </form>    
+        </div>  
+    </body>
