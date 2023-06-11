481f8f704c12122b25e6cab1a90b30d5ebac8c1c
Stefan Khor <stefankhor2002@gmail.com>
2023-06-11 15:13:47 +0800

    created courseDetail

---
+++ courseDetail/courseDetail.php
@@ -0,0 +1,32 @@
+<?php include '../db_connect.php'; ?>
+
+<html>
+
+<head>
+    <title>Course Details</title>
+    <link rel="stylesheet" type="text/css" href = "courseDetail.css">
+</head>
+
+<body>
+    <!-- nav bar and stuff -->
+    <div class="header">
+    </div>
+
+    <div class="banner">
+        <img src="?" alt="Course img">
+        <h1> Cloud Computing </h1>
+        <p>1/6/2023 - 20/9/2023</p>
+    </div>
+
+</div class="announcement">
+    <h1> Announcements </h1>
+    <div class="eachAnnouncement">
+        <img src="*" alt="profile picture">
+        <h3>Author</h3>
+        <p>Time</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore illum accusantium molestias commodi voluptatum perspiciatis quam, est corporis! Accusantium quasi facilis, enim nihil quia nulla nesciunt saepe alias cum ea? </p>
+    </div>
+</div>
+</body>
+
+</html>
