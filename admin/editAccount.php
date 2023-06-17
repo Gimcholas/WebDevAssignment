5f706c3a439b8e961dd152f406d65380944f5d54
JJ <jiajianmmu@gmail.com>
2023-06-17 21:49:03 +0800

    added php code to validate and update userdata

--- admin/editAccount.php
+++ admin/editAccount.php
@@ -108,7 +108,10 @@
             </div> 
             
             <div class='submitBtn'>
-                <input type='submit' name='submit' value='Save'>
+                <input type='submit' name='submit' value='Save'><br><br>
+            </div>
+            <div class='backBtn'>
+                <a href="dashboard.php"><input type='button' name='back' value='Back'></a>
             </div>
             </form>
             <?php
@@ -120,19 +123,71 @@
 <?php
 
 if(isset($_POST['submit'])) {
-    if($usertype == 'Admin') {
-
-    }
-    else if($usertype == 'Instructor') {
-
-    }
-    else if($usertype == 'Student') {
-
-    }
-    else if($usertype == 'Provider') {
-
+    // User change password
+    if(!empty($_POST["password"])) {
+        if(strlen($_POST["password"]) < 8) {
+            die("Password must be at least 8 characters"); 
+        }
+        if (!preg_match('/[A-Za-z]/', $_POST["password"]) || !preg_match('/[0-9]/', $_POST["password"])) {
+            die("Password must include at least one letter and one number");
+        }
+        
+        // Update password in the database
+        $password = $_POST["password"];
+        $hashed_password = password_hash($password,PASSWORD_DEFAULT);
+        $sql = "UPDATE user SET password_hash = '" . $hashed_password . "' where username = " . "'" . $username . "';";
+        $result = mysqli_query($connect,$sql);
+        if (!$result) {
+            echo "Error: " . mysqli_error($connect);
+        }
     }
     
+
+    // Admin can only edit password
+
+
+
+    if($usertype == 'Instructor' || $usertype == 'Student' || $usertype == 'Provider') {
+        $contactNumber = $_POST["contactNumber"];
+        $email = $_POST["email"];
+    }
+
+    if($usertype == 'Instructor') {
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        
+        $sql2 = "UPDATE instructor SET first_name = '" . $firstName . "', last_name = '" . $lastName 
+                . "', contact_number = '" . $contactNumber . "', email ='" . $email 
+                . "' WHERE username = '" . $username . "';";
+   
+        $result = mysqli_query($connect,$sql2);
+    }
+    else if($usertype == 'Student') {
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $dateOfBirth = $_POST["dateOfBirth"];
+        $academicProgram = $_POST["academicProgram"];
+        $sql2 = "UPDATE student SET first_name = '" . $firstName . "', last_name = '" . $lastName 
+                . "', date_of_birth = '" . $dateOfBirth ."', academic_program = '". $academicProgram 
+                . "',contact_number = '" . $contactNumber . "', email ='" . $email 
+                . "' WHERE username = '" . $username . "';";
+        $result = mysqli_query($connect,$sql2);
+    }
+    else if($usertype == 'Provider') {
+        $providerName = $_POST['providerName'];
+        $sql2 = "UPDATE training_provider SET provider_name = '" . $providerName . "', contact_number ='" 
+                . $contactNumber . "', email='" . $email 
+                . "' WHERE username='" . $username ."';";
+        $result = mysqli_query($connect,$sql2);
+    }
+    ?>
+
+    <script>
+		alert("User <?php echo $username.' Updated!';?>");
+	</script>
+
+    <?php
+    header( "refresh:0.5; url=dashboard.php" );
 }
 
 ?>
