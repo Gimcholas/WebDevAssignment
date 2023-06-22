c38809d05015faa4ecc365c262e46a983d949d7f
JJ <jiajianmmu@gmail.com>
2023-06-22 17:55:09 +0800

    Modified: Direct to editCourse.php instead of JS

--- trainingProvider/courseDetail.php
+++ trainingProvider/courseDetail.php
@@ -1,6 +1,7 @@
 <?php 
     include "../db_connect.php";
     include "functions.php";
+    $_SESSION["username"] = "Huawei";
     
 ?>
 
@@ -11,26 +12,27 @@
 
 <body>
     <?php 
-    if(isset($_GET['course'])) {
+    if(isset($_GET["view"])) {
+        if (isset($_GET['course'])) {
         $course = getCourse($_GET['course'],$connect);
         $courseSectionResult = getCourseSectionsResult($_GET['course'],$connect);
     ?>
     <div class='course-details'>
         <h2>Course Details</h2>
         <?php 
-        echo "<a id='". $_GET['course'] . "' onclick='editCourseDetails(this)'><button>Edit</button></a>";
-        echo "<p>Course ID: <span id='course-id'>" . $course['course_id'] . "</span></p>";
-        echo "<p>Course Title: <span id='course-title'>" . $course['course_title'] . "</span></p>";
-        echo "<p>Course Description: <span id='course-description'>" . $course['course_description'] . "</span></p>";
-        echo "<p>Start Date: <span id='start-date'>" . $course['start_date'] . "</span></p>";
-        echo "<p>End Date: <span id='end-date'>" . $course['end_date'] . "</span></p>";
+        echo "<a href='editCourse.php?edit&course=" .$_GET['course'] ."'><button>Edit</button></a>";
+        echo "<a href='courses.php'><button>Back</button></a>";
+        echo "<p>Course ID: ". $course['course_id'] . "</p>";
+        echo "<p>Course Title: " . $course['course_title'] . "</p>";
+        echo "<p>Course Description: " . $course['course_description'] . "</p>";
+        echo "<p>Start Date: " . $course['start_date'] . "</p>";
+        echo "<p>End Date: " . $course['end_date'] . "</p>";
         ?>
     </div>
     <hr>
     <?php
     while ($row = mysqli_fetch_assoc($courseSectionResult)) {
         echo "<div class='course-section-details'>";
-        echo "<a id=". $row['course_section_id'] . " onclick=><button>Edit</button></a>";
         echo "<p>Course Section ID: ". $row['course_section_id']."</p>";
         echo "<p>Course Section Name: ". $row['course_section_name']."</p>";
         echo "<p>Instructor Username: ". $row['username']."</p>";
@@ -40,49 +42,11 @@
         echo "<p>Day: ". $row['day'] . "</p>";
         echo "<p>Status: ". $row['status'] ."</p>";
         echo "<p>Maximum Students Allowed: ". $row['max_student_num'] . "</p>";
-        
-
-        //echo "<p>Course Section ID: ". $row['course_section_id']."</p>";
         echo "</div>";
         echo "<hr>";
     }
     }
+}
     ?>
 </body>
 </html>
-
-<script>
-    function editCourseDetails(editButton) {
-        var courseDetails = editButton.parentNode;
-        var spans = courseDetails.getElementsByTagName('span');
-
-        for (var i = 0; i < spans.length; i++) {
-            var span = spans[i];
-            var input = document.createElement('input');
-            input.value = span.innerText;
-            span.innerHTML = '';
-            span.appendChild(input);
-        }
-
-        editButton.innerHTML = 'Save';
-        editButton.onclick = function() {
-            saveCourseDetails(editButton);
-        };
-    }
-
-    function saveCourseDetails(saveButton) {
-        var courseDetails = saveButton.parentNode;
-        var spans = courseDetails.getElementsByTagName('span');
-
-        for (var i = 0; i < spans.length; i++) {
-            var span = spans[i];
-            var input = span.firstChild;
-            span.innerHTML = input.value;
-        }
-
-        saveButton.innerHTML = 'Edit';
-        saveButton.onclick = function() {
-            editCourseDetails(saveButton);
-        };
-    }
-</script>
