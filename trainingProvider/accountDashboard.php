9d1a67786aaf48d0dcc81d626ba1b4ff976e228e
JJ <jiajianmmu@gmail.com>
2023-06-26 22:54:07 +0800

    removed delete user button

--- trainingProvider/accountDashboard.php
+++ trainingProvider/accountDashboard.php
@@ -30,7 +30,6 @@
         <th colspan="2">Action</th>
     </tr>
     <?php
-    //$sql = "SELECT * FROM user ORDER BY joined_date;";
     $sql = "SELECT * FROM user INNER JOIN instructor on user.username = instructor.username WHERE provider_username = '". $_SESSION["username"] . "';";
     $result = mysqli_query($connect,$sql);
     $countInstructor = mysqli_num_rows($result);
@@ -44,9 +43,8 @@
         <td><?php echo $row["contact_number"]; ?></td>
         <td><?php echo $row["email"]; ?></td>
         <td><?php echo $row["joined_date"]; ?></td>
-        <td><a href="accountDetail.php?view&username=<?php echo $row["username"];?>">Details</a></td>
+        <td><a href="../admin/accountDetail.php?view&username=<?php echo $row["username"];?>">Details</a></td>
         <td><a href="editAccount.php?edit&username=<?php echo $row["username"];?>">Edit</a></td>
-        <!-- <td><a href="dashboard.php?del&username=<?php echo $row["username"];?>" onclick="return confirmation();">Delete</a></td> -->
     </tr>
     <?php
     }
@@ -71,7 +69,6 @@
         <th colspan="2">Action</th>
     </tr>
     <?php
-    //$sql = "SELECT * FROM user ORDER BY joined_date;";
     $sql = "SELECT * FROM user INNER JOIN student on user.username = student.username WHERE provider_username = '". $_SESSION["username"] . "';";
     $result = mysqli_query($connect,$sql);
     $countStudent = mysqli_num_rows($result);
@@ -87,9 +84,8 @@
         <td><?php echo $row["contact_number"]; ?></td>
         <td><?php echo $row["email"]; ?></td>
         <td><?php echo $row["joined_date"]; ?></td>
-        <td><a href="accountDetail.php?view&username=<?php echo $row["username"];?>">Details</a></td>
+        <td><a href="../admin/accountDetail.php?view&username=<?php echo $row["username"];?>">Details</a></td>
         <td><a href="editAccount.php?edit&username=<?php echo $row["username"];?>">Edit</a></td>
-        <!-- <td><a href="dashboard.php?del&username=<?php echo $row["username"];?>" onclick="return confirmation();">Delete</a></td> -->
     </tr>
     <?php
     }
@@ -105,49 +101,3 @@
 </body>
 
 </html>
-
-<?php
- 
-    if(isset($_GET["del"])) {
-        $username = $_GET['username'];
-        // find the usertype
-        $sql = "SELECT * from user where username ='" . $username . "';";
-        $result = mysqli_query($connect,$sql);
-        $count = mysqli_num_rows($result);
-        if ($count == 0) {
-            ?>
-            <script>
-            alert( "User '<?php echo $username ?>' not found ");
-            window.location.href="dashboard.php";
-            </script>
-            <?php
-        }
-        else {
-            $row = mysqli_fetch_assoc($result);
-            if($row["usertype"] == 'Instructor') {
-                $sql = "DELETE from instructor WHERE username ='" . $username . "';";
-                $result = mysqli_query($connect,$sql);
-            }
-            else if($row["usertype"] == 'Student') {
-                $sql = "DELETE from student WHERE username ='" . $username . "';";
-                $result = mysqli_query($connect,$sql);
-            }
-            else if($row["usertype"] == 'Provider') {
-                $sql = "DELETE from training_provider WHERE username ='" . $username . "';";
-                $result = mysqli_query($connect,$sql);
-            }
-            
-            $sql = "DELETE from user WHERE username ='" . $username . "';";
-            $result = mysqli_query($connect,$sql);
-            ?>
-
-            <script>
-            alert( "User '<?php echo $username ?>' has been deleted.");
-            window.location.href="dashboard.php";
-            </script>;
-
-            <?php
-        }
-        //header("Location: dashboard.php");
-    }
-?>
