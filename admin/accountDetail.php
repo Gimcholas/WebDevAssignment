fad2ea86b115e1e2229a046146ca4778e3175806
JJ <jiajianmmu@gmail.com>
2023-06-26 22:51:12 +0800

    + View user registered course/ course in charge

--- admin/accountDetail.php
+++ admin/accountDetail.php
@@ -35,6 +35,22 @@
 		    echo $row["joined_date"]; 
 
             if($row["usertype"] == "Instructor") {
+                
+                $sql = "SELECT AVG(rating) as rating, COUNT(rating) as amount FROM instructor_feedback where instructor_username = '$username'";
+                $result = mysqli_query($connect,$sql);
+                $row = mysqli_fetch_assoc($result);
+                $numOfStars = floor($row["rating"]);
+                echo "<div class=rating>";
+                echo "<b>Rating</b>";
+                for($i=0; $i<$numOfStars; $i++)
+                    echo "<img src='../files/star-orange.png' alt='star'>";
+                for($i=0; $i<(5-$numOfStars); $i++)
+                    echo "<img src='../files/star-white.png' alt='star'>";
+            
+                echo " (" . $row["amount"] . ")";
+                echo "</div>"; 
+                
+
                 $sql = "SELECT * FROM instructor JOIN training_provider ON instructor.provider_username = training_provider.username where instructor.username='$username';";
                 $result = mysqli_query($connect,$sql);
                 $row = mysqli_fetch_assoc($result);
@@ -51,9 +67,50 @@
 		        echo $row["contact_number"]; 
                 echo "<br><b>Email</b><br>";
 		        echo $row["email"]; 
+                
+
+                echo "<h3>Courses</h3>"; ?>
+                
+                <?php 
+                $sql = "SELECT * FROM instructor JOIN training_provider ON 
+                        instructor.provider_username = training_provider.username 
+                        JOIN course_section ON course_section.username = instructor.username
+                        JOIN course ON course_section.course_id = course.course_id
+                        where instructor.username='" . $username ."';";
+
+                $result = mysqli_query($connect,$sql); 
+                $numOfRows = mysqli_num_rows($result);
+                if($numOfRows == 0) {
+                    echo "No Course In Charge";
+                }
+                else {
+                ?>
+                <table border="1">
+                <tr>
+                    <th>Course ID</td>
+                    <th>Course Title</td>
+                    <th>Course Section</td>
+                    <th>Day</td>
+                    <th>Time</td>
+                </tr>
+                <?php
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row["course_id"]. "</td>";
+                    echo "<td>" . $row["course_title"]. "</td>";
+                    echo "<td>" . $row["course_section_name"] . "</td>";
+                    echo "<td>" . $row["day"] . "</td>";
+                    echo "<td>" . date("h:i:a",strtotime($row["start_time"])) . " - " . date("h:i:a",strtotime($row["end_time"])) ."</td>";
+                    echo "</tr>";
+                }
+            }
+
             }
             else if($row["usertype"] == "Student") {
-                $sql = "SELECT * FROM student JOIN training_provider ON student.provider_username = training_provider.username where student.username='$username';";
+                $sql = "SELECT * FROM student JOIN training_provider ON 
+                        student.provider_username = training_provider.username 
+                        where student.username='" . $username ."';";
+
                 $result = mysqli_query($connect,$sql);
                 $row = mysqli_fetch_assoc($result);
 
@@ -73,6 +130,44 @@
 		        echo $row["contact_number"]; 
                 echo "<br><b>Email</b><br>";
 		        echo $row["email"]; 
+                ?>
+                <h3>Registered Course</h3>
+                <?php 
+                $sql = "SELECT * FROM student JOIN training_provider ON 
+                        student.provider_username = training_provider.username 
+                        JOIN course_student ON course_student.username = student.username
+                        JOIN course_section ON course_student.course_section_id = course_section.course_section_id
+                        JOIN course ON course_section.course_id = course.course_id
+                        where student.username='" . $username ."';";
+
+                $result = mysqli_query($connect,$sql);
+                $numOfRows = mysqli_num_rows($result);
+                if ($numOfRows == 0) {
+                    echo "<p>No course found</p>";
+                }
+                else {
+                echo "<table border='1'>";
+                echo "<tr>";
+                echo "<th>Course Title</th>";
+                echo "<th>Course Section Name</th>";
+                echo "<th>Day</th>";
+                echo "<th>Time</th>";
+                echo "<th>Completed Date</th>";
+                echo "</tr>";
+                    
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row["course_title"] . "</td>";
+                    echo "<td>" . $row["course_section_name"] . "</td>";
+                    echo "<td>" . $row["day"] . "</td>";
+                    echo "<td>" . date("h:i:a",strtotime($row["start_time"])) . " - " . date("h:i:a",strtotime($row["end_time"])) ."</td>";
+                    echo "<td>" . $row["course_completed_date"] . "</td>";
+                    echo "</tr>";
+                }
+                }
+
+                ?>
+                <?php
             }
             else if($row["usertype"] == "Provider") {
                 $sql = "SELECT * FROM training_provider where username='$username';";
