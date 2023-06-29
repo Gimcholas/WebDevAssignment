737cdf62e83c77cdf2d1641e8ccb145bc343e7bb
JJ <jiajianmmu@gmail.com>
2023-06-29 18:13:44 +0800

    Created profile page for admin

--- admin/profile.php
+++ admin/profile.php
@@ -1,27 +1,4 @@
-<?php include '../db_connect.php';
-session_start();
+<?php 
+  include '../phpFunction/function.php';
+  generatePage("Profile",'createProfilePage','<link rel="stylesheet" type="text/css" href="../css/profile.css">','<script src ="../js/profile.js"></script>');
 ?>
-
-<!DOCTYPE html>
-<html>
-<head> 
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../navBar/navBarStyle.css"/>
-    <link rel="stylesheet" href="#"/>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src ="../js/navbar.js"></script></head>
-
-<body>
-<div class="Container">
-    <div class="sidebar">
-        <?php include '../navBar/navBar.php'?>
-    </div>
-    <div class="content" id="content"> 
-        <!-- do profile code here  -->
-        
-    </div>
-</div>
-</body>
-
-
-</html>
