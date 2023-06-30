ea06bce11dc32b23da3311df1a6ddd1dc1a8b57a
wengkee101 <wengkee101@gmail.com>
2023-06-30 19:18:29 +0800

    CSS Changed

--- trainingProvider/editCourse.php
+++ trainingProvider/editCourse.php
@@ -150,185 +150,194 @@
 <head>
     <title>Edit Course Details</title>
     <script text="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="../navBar/navBarStyle.css"/>
+    <link rel="stylesheet" href="../css/editCourse.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src ="../js/navbar.js"></script></head>
 </head>
 
 <body>
-    <h1>Edit Course</h1>
-    <?php 
-    if(isset($_GET["edit"])) {
-        if(isset($_GET["course"])) {
-            $course = getCourse($_GET['course'],$connect);
-            $providerUsername = $course["provider_username"];
-            $courseSectionResult = getCourseSectionsResult($_GET['course'],$connect);
-            ?>
-            <form action="" method="post" id="courseForm" enctype="multipart/form-data">
-                <input type="text" name="courseID" value = <?php echo $course["course_id"]?> hidden>
-                <div class="course-container">
-                    <div class="input-box">
-                    <label for="courseName">Course Name</label>
-                    <input type="text" name="courseName" required value = "<?php echo $course['course_title']; ?>"> 
-                    </div><br>
+<div class="Container">
+    <div class="sidebar">
+        <?php include '../NavBar/NavBar.php'?>
+    </div>
+    <div class="content" id="content"> 
+        <header>
+            <h1>Edit Course</h1>
+        </header>
+        <?php 
+        if(isset($_GET["edit"])) {
+            if(isset($_GET["course"])) {
+                $course = getCourse($_GET['course'],$connect);
+                $providerUsername = $course["provider_username"];
+                $courseSectionResult = getCourseSectionsResult($_GET['course'],$connect);
+                ?>
+                <div class="Container1">
+                    <form action="" method="post" id="courseForm" enctype="multipart/form-data">
+                        <input type="text" name="courseID" value = <?php echo $course["course_id"]?> hidden>
+                        <div class="course-container">
+                            <div class="input-box">
+                            <label for="courseName">Course Name</label>
+                            <input type="text" name="courseName" required value = "<?php echo $course['course_title']; ?>"> 
+                            </div>
 
-                    <div class="input-box">
-                    <label for="startDate">Start Date</label>
-                    <input type="date" name="startDate" required value = <?php echo $course['start_date']; ?>> 
-                    </div><br>
+                            <div class="input-box">
+                            <label for="startDate">Start Date</label>
+                            <input type="date" name="startDate" required value = <?php echo $course['start_date']; ?>> 
+                            </div>
 
-                    <div class="input-box">
-                    <label for="endDate">End Date</label>
-                    <input type="date" name="endDate" required value = <?php echo $course['end_date']; ?>> 
-                    </div><br>
+                            <div class="input-box">
+                            <label for="endDate">End Date</label>
+                            <input type="date" name="endDate" required value = <?php echo $course['end_date']; ?>> 
+                            </div>
 
-                    <div class="input-box">
-                    <label for="courseIntro">Course Introduction</label>
-                    
-                    <textarea name="courseIntro" rows="5" cols="50"><?php echo $course['course_description']; ?></textarea>
-                    </div><br>
+                            <div class="input-box">
+                            <label for="courseIntro">Course Introduction</label>
+                            
+                            <textarea name="courseIntro" rows="5" cols="50"><?php echo $course['course_description']; ?></textarea>
+                            </div>
 
-                    <div class="input-box">
-                    <label for="Course Image">Upload course image</label><br>
-                    <input type="file" name="photo">
-                    </div><br>
-                </div>
-
-            <div class=course-sections-container>
-            <?php 
-            $numOfSections = mysqli_num_rows($courseSectionResult);
-            if ($numOfSections == 0) { ?>
-                <div class=course-section>
-                <div class="input-box">
-                <label for="sectionName">Section Name</label>
-                <input type="text" name="sectionName[]" required>
-                </div><br>
-                   
-                <div class="input-box">
-                <label for="instructorUsername">Instructor Username</label>
-                <select name="instructorUsername[]" id="originalSelector" required value>
-                <!-- Get instructor list -->
-                <?php 
-                        $sql = "SELECT * FROM instructor where provider_username" . "= '" . $providerUsername . "';";
-                        $result = mysqli_query($connect,$sql);
-                        $count = mysqli_num_rows($result);
-                        if ($count == 0) {
-                            echo $count;
-                            ?>
-                            <option disabled selected value>No Available Instructors Found</option>
-    
-                        <?php
-                        }
-                        while($row2 = mysqli_fetch_array($result)) {
-                         ?>    
-                            <option value=<?php echo "'" . $row2["username"] . "' ";?>>
-                                    <?php echo $row2["username"] . " - " . $row2["first_name"] . " " . $row2["last_name"]?>
-                            </option> 
-                        <?php }
-                        ?>
-                </select>
-                </div><br>
-
-                <div class="input-box">
-                    <label for="maxStudentNum">Maximum Student Allowed</label>
-                    <input type="number" name="maxStudentNum[]" required>
-                </div><br>
-            </div>
-            <?php
-            }
-            while($row = mysqli_fetch_assoc($courseSectionResult)) { ?>
-                <div class=course-section>
-                <input type="text" name="courseSectionID[]" value = <?php echo $row["course_section_id"]?> hidden>
-                <div class="input-box">
-                <label for="sectionName">Section Name</label>
-                <input type="text" name="sectionName[]" required value="<?php echo $row["course_section_name"]?>">
-                </div><br>
-    
-    
-                
-                <div class="input-box">
-                <label for="instructorUsername">Instructor Username</label>
-                <select name="instructorUsername[]" id="originalSelector" required value>
-                <!-- Get instructor list -->
-                <?php 
-                        $sql = "SELECT * FROM instructor where provider_username" . "= '" . $providerUsername . "';";
-                        $result = mysqli_query($connect,$sql);
-                        $count = mysqli_num_rows($result);
-                        if ($count == 0) {
-                            echo $count;
-                            ?>
-                            <option disabled selected value>No Available Instructors Found</option>
-    
-                        <?php
-                        }
-                        while($row2 = mysqli_fetch_array($result)) {
-                         ?>    
-                            <option value=<?php echo "'" . $row2["username"] . "' "; if ($row2["username"] == $row["username"]) echo " selected";?>>
-                                    <?php echo $row2["username"] . " - " . $row2["first_name"] . " " . $row2["last_name"]?>
-                            </option> 
-                        <?php }
-                        ?>
-                </select>
-                </div><br>
-    
-                <div class="input-box">
-                <label for="startTime">Start Time</label>
-                <input type="time" name="startTime[]" value = <?php echo $row["start_time"]?>>
-                </div><br>
-    
-                <div class="input-box">
-                <label for="endTime">End Time</label>
-                <input type="time" name="endTime[]" value = <?php echo $row["end_time"]?>>
-                </div><br>
-    
-                <div class="input-box">
-                <label for="day">Day</label>
-                <select name="day[]">
-                    <option disabled hidden value="" selected>Choose a day</option>
-                    <option value="Sunday" <?php if ($row["day"] == 'Sunday') echo ' selected'; ?>>Sunday</option>
-                    <option value="Monday" <?php if ($row["day"] == 'Monday') echo ' selected'; ?>>Monday</option>
-                    <option value="Tuesday"<?php if ($row["day"] == 'Tuesday') echo ' selected'; ?>>Tuesday</option>
-                    <option value="Wednesday"<?php if ($row["day"] == 'Wednesday') echo ' selected'; ?>>Wednesday</option>
-                    <option value="Thursday"<?php if ($row["day"] == 'Thursday') echo ' selected'; ?>>Thursday</option>
-                    <option value="Friday"<?php if ($row["day"] == 'Friday') echo ' selected'; ?>>Friday</option>
-                    <option value="Saturday"<?php if ($row["day"] == 'Saturday') echo ' selected'; ?>>Saturday</option>
-                </select>    
-                </div><br>
-    
-                <div class="input-box">
-                <label for="status">Status</label>
-                <select name="status[]" required>
-                    <option disabled hidden value="" selected>Choose a status</option>
-                    <option value="Open" <?php if ($row["status"] == 'Open') echo ' selected';?> >Open</option>
-                    <option value="Close" <?php if ($row["status"] == 'Closed') echo ' selected';?> >Closed</option>
-                </select>    
-                </div><br>
-
-                <div class="input-box">
-                    <label for="maxStudentNum">Maximum Student Allowed</label>
-                    <input type="number" name="maxStudentNum[]" required value=<?php echo $row["max_student_num"]?>>
-                </div><br>
-            </div>
-
-            <?php }
-            ?>
-            </div>
-
-            <div id="additionalSection"></div>
-        
-            <div class="addSection">
-                <input type="button" value="Add More Section" onclick="addSection()">     
-            </div>
-
-            <a href="courseDetail.php?view&course=<?php echo $_GET["course"]?>">Cancel</a>
-            <input type="submit" name="updateCourse" value="Save">
+                            <div class="input-box">
+                            <label for="Course Image">Upload course image</label><br>
+                            <input type="file" name="photo">
+                            </div>
+                        </div>
+                        <hr>
+                    <div class=course-sections-container>
+                    <?php 
+                    $numOfSections = mysqli_num_rows($courseSectionResult);
+                    if ($numOfSections == 0) { ?>
+                        <div class=course-section>
+                        <div class="input-box">
+                        <label for="sectionName">Section Name</label>
+                        <input type="text" name="sectionName[]" required>
+                        </div>
+                        
+                        <div class="input-box">
+                        <label for="instructorUsername">Instructor Username</label>
+                        <select name="instructorUsername[]" id="originalSelector" required value>
+                        <!-- Get instructor list -->
+                        <?php 
+                                $sql = "SELECT * FROM instructor where provider_username" . "= '" . $providerUsername . "';";
+                                $result = mysqli_query($connect,$sql);
+                                $count = mysqli_num_rows($result);
+                                if ($count == 0) {
+                                    echo $count;
+                                    ?>
+                                    <option disabled selected value>No Available Instructors Found</option>
             
-            </form>
-        
+                                <?php
+                                }
+                                while($row2 = mysqli_fetch_array($result)) {
+                                ?>    
+                                    <option value=<?php echo "'" . $row2["username"] . "' ";?>>
+                                            <?php echo $row2["username"] . " - " . $row2["first_name"] . " " . $row2["last_name"]?>
+                                    </option> 
+                                <?php }
+                                ?>
+                        </select>
+                        </div>
 
-    <?php
+                        <div class="input-box">
+                            <label for="maxStudentNum">Maximum Student Allowed</label>
+                            <input type="number" name="maxStudentNum[]" required>
+                        </div>
+                    </div>
+                    <?php
+                    }
+                    while($row = mysqli_fetch_assoc($courseSectionResult)) { ?>
+                        <div class=course-section>
+                        <input type="text" name="courseSectionID[]" value = <?php echo $row["course_section_id"]?> hidden>
+                        <div class="input-box">
+                        <label for="sectionName">Section Name</label>
+                        <input type="text" name="sectionName[]" required value="<?php echo $row["course_section_name"]?>">
+                        </div>
+            
+                        <div class="input-box">
+                        <label for="instructorUsername">Instructor Username</label>
+                        <select name="instructorUsername[]" id="originalSelector" required value>
+                        <!-- Get instructor list -->
+                        <?php 
+                                $sql = "SELECT * FROM instructor where provider_username" . "= '" . $providerUsername . "';";
+                                $result = mysqli_query($connect,$sql);
+                                $count = mysqli_num_rows($result);
+                                if ($count == 0) {
+                                    echo $count;
+                                    ?>
+                                    <option disabled selected value>No Available Instructors Found</option>
+            
+                                <?php
+                                }
+                                while($row2 = mysqli_fetch_array($result)) {
+                                ?>    
+                                    <option value=<?php echo "'" . $row2["username"] . "' "; if ($row2["username"] == $row["username"]) echo " selected";?>>
+                                            <?php echo $row2["username"] . " - " . $row2["first_name"] . " " . $row2["last_name"]?>
+                                    </option> 
+                                <?php }
+                                ?>
+                        </select>
+                        </div>
+            
+                        <div class="input-box">
+                        <label for="startTime">Start Time</label>
+                        <input type="time" name="startTime[]" value = <?php echo $row["start_time"]?>>
+                        </div>
+            
+                        <div class="input-box">
+                        <label for="endTime">End Time</label>
+                        <input type="time" name="endTime[]" value = <?php echo $row["end_time"]?>>
+                        </div>
+            
+                        <div class="input-box">
+                        <label for="day">Day</label>
+                        <select name="day[]">
+                            <option disabled hidden value="" selected>Choose a day</option>
+                            <option value="Sunday" <?php if ($row["day"] == 'Sunday') echo ' selected'; ?>>Sunday</option>
+                            <option value="Monday" <?php if ($row["day"] == 'Monday') echo ' selected'; ?>>Monday</option>
+                            <option value="Tuesday"<?php if ($row["day"] == 'Tuesday') echo ' selected'; ?>>Tuesday</option>
+                            <option value="Wednesday"<?php if ($row["day"] == 'Wednesday') echo ' selected'; ?>>Wednesday</option>
+                            <option value="Thursday"<?php if ($row["day"] == 'Thursday') echo ' selected'; ?>>Thursday</option>
+                            <option value="Friday"<?php if ($row["day"] == 'Friday') echo ' selected'; ?>>Friday</option>
+                            <option value="Saturday"<?php if ($row["day"] == 'Saturday') echo ' selected'; ?>>Saturday</option>
+                        </select>    
+                        </div>
+            
+                        <div class="input-box">
+                        <label for="status">Status</label>
+                        <select name="status[]" required>
+                            <option disabled hidden value="" selected>Choose a status</option>
+                            <option value="Open" <?php if ($row["status"] == 'Open') echo ' selected';?> >Open</option>
+                            <option value="Close" <?php if ($row["status"] == 'Closed') echo ' selected';?> >Closed</option>
+                        </select>    
+                        </div>
+
+                        <div class="input-box">
+                            <label for="maxStudentNum">Maximum Student Allowed</label>
+                            <input type="number" name="maxStudentNum[]" required value=<?php echo $row["max_student_num"]?>>
+                        </div>
+                        <hr>
+                    </div>
+
+                    <?php }
+                    ?>
+                    </div>
+
+                    <div id="additionalSection"></div>
+                
+                    <div class="addSection">
+                        <input type="button" style="display:inline-block;" value="Add More Section" onclick="addSection()">     
+                        <input type="submit" style="display:inline-block;" name="updateCourse" value="Save">
+                        <a href="courseDetail.php?view&course=<?php echo $_GET["course"]?>"><input type="button" style="display:inline-block;" value="Cancel"></a>
+                    </div>
+                    
+                    </form>
+            </div>
+        <?php
+            }
         }
-    }
-    ?>
-
+        ?>
+    </div>
+</div>
 </body>
 </html>
-<script>
-</script>
+
