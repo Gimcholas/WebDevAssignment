09587ab4d25f289c8e7e2dd091f0cf4741a60bd1
Stefan Khor <stefankhor2002@gmail.com>
2023-07-02 16:48:36 +0800

    changed to tpms

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $connect= mysqli_connect("localhost","root","","assignment");
+    $connect= mysqli_connect("localhost","root","","tpms");
     if(!$connect)
         die('Could not connect to database:'.mysqli_error($connect));
 ?>
